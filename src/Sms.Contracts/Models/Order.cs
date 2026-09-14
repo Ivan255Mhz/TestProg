ed31e9e54a2313b8a8c19f249687ecf9f330d75f
Ivan <iivansamiko1996gmail.com>
2026-09-14 20:10:42 +0300

    fea(task1): начало работы , добавлены модели в проект

---
+++ src/Sms.Contracts/Models/Order.cs
@@ -0,0 +1,10 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Sms.Contracts.Models
+{
+    public record Order(IReadOnlyList<OrderItem> Items);
+}
---
+++ src/Sms.Contracts/Transport/ApiResponse.cs
@@ -0,0 +1,10 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Sms.Contracts.Transport
+{
+    public record ApiResponse(bool Success, string? ErrorMessage, object? Data);
+}
